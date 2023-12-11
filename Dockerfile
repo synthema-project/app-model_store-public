d1b18bb67805a7b253301f86e2457be4cbc3f0c0
Borja <s.barrio@upm.es>
2023-12-11 15:47:47 +0000

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
 EXPOSE 8000
-CMD python main.py mlflow=test_upm
+CMD python main.py
--- config.py
+++ config.py
@@ -1,22 +1,10 @@
 import mlflow as mlf
+import os
 
-# @@@@@@@ KC SETTINGS @@@@@@
-# realm = os.getenv('KEYCLOAK_REALM')
-# keycloack_url = os.getenv('KEYCLOAK_URL')
-# client_id = os.getenv('KEYCLOAK_CLIENT_ID')
-# authorization_url = keycloack_url + "realms/" + realm + "/protocol/openid-connect/auth"
-# token_url = keycloack_url + "realms/" + realm + "/protocol/openid-connect/token"
-# keycloack_admin = os.getenv('KEYCLOAK_ADMIN')
-from hydra import initialize, compose
+TRACKING_URI = os.environ['TRACKING_URI']
+HOST = os.environ['HOST']
+PORT = int(os.environ['PORT'])
 
-initialize(version_base=None, config_path="conf")
-cfg = compose(config_name="config")
-TRACKING_URI = cfg["mlflow"]["ip"]
-HOST = cfg["app"]["host"]
-PORT = cfg["app"]["port"]
-
-# t_cfg = compose(config_name="test_upm")
-# FTRACKING_URI=t_cfg["ip"]
 
 mlf.set_tracking_uri(TRACKING_URI)
 client = mlf.MlflowClient(tracking_uri=TRACKING_URI)
