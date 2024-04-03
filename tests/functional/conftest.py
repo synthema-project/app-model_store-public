a96222f76b623074fc0237d171f32d8fd76a527b
Borja Arroyo Galende <bag@gatv.ssr.upm.es>
2024-04-03 16:02:24 +0200

    Add PYTHONPATH, dockerignore and change structure

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from mlflow.pyfunc import PythonModel
 from pathlib import Path
 
-from src.config import config
+from src import config
 
 
 def pytest_addoption(parser):
@@ -19,12 +19,9 @@ def monkeysession(request):
 def mock_mlflow_uri(monkeysession, pytestconfig):
     env = pytestconfig.getoption("--env")
     if env == "local":
-        project_dir = Path(__file__).resolve().parents[3]
-        mock_uri = f"file://{project_dir}/mlruns"
-        monkeysession.setenv("MLFLOW_TRACKING_URI", mock_uri)
         config.mlflow_client = config.load_mlflow_client()
         yield
-        shutil.rmtree(mock_uri)
+        shutil.rmtree("mlruns")
     elif env == "local-k8":
         raise NotImplementedError("Kubernetes environment not implemented")
     else:
