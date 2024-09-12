39e81808208fecd2bfba965daa6605b62ff63209
sblottmann <sbl@gatv.ssr.upm.es>
2024-09-12 14:09:34 +0200

    debug

--- tests/functional/test_f_models.py
+++ tests/functional/test_f_models.py
@@ -7,6 +7,8 @@ from src.main import create_app
 
 client = TestClient(create_app())
 
+def test_bucket_name():
+    assert os.getenv('MLFLOW_S3_BUCKET') == 'mlflow'
 
 @pytest.fixture(scope="module")
 def setup_environment():
@@ -14,22 +16,24 @@ def setup_environment():
     yield
     del os.environ["MLFLOW_S3_BUCKET"]
 
-# @pytest.fixture(scope="module")
-# def test_upload_model(model_generator, mock_mlflow_uri, setup_environment):
-#     model_name = "test_model"
-#     disease = "AML"
-#     description = "Test description"
-#
-#     file = cloudpickle.dumps(model_generator)
-#
-#     response = client.post(
-#         "/models/upload",
-#         data={"model_name": model_name, "disease": disease, "description": description},
-#         files={"file": ("test_file.pkl", file, "application/octet-stream")},
-#     )
-#     assert response.status_code == 201, f"Failed with response: {response.text}"
-#     assert response.json()["detail"] == f"Model '{model_name}' registered."
-#     yield
+@pytest.fixture(scope="module")
+def test_upload_model(model_generator, mock_mlflow_uri, setup_environment):
+    model_name = "test_model"
+    disease = "AML"
+    description = "Test description"
+
+    file = cloudpickle.dumps(model_generator)
+
+    test_bucket_name()
+
+    response = client.post(
+        "/models/upload",
+        data={"model_name": model_name, "disease": disease, "description": description},
+        files={"file": ("test_file.pkl", file, "application/octet-stream")},
+    )
+    assert response.status_code == 201, f"Failed with response: {response.text}"
+    assert response.json()["detail"] == f"Model '{model_name}' registered."
+    yield
 
 def test_download_model(test_upload_model, mock_mlflow_uri, setup_environment):
     model_name = "test_model"
