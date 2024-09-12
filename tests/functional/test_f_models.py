a7126176cb823dc43e5c30861caf38df9de72b12
sblottmann <sbl@gatv.ssr.upm.es>
2024-09-12 13:55:17 +0200

    debug

--- tests/functional/test_f_models.py
+++ tests/functional/test_f_models.py
@@ -26,14 +26,14 @@ def test_upload_model(model_generator, mock_mlflow_uri, setup_environment):
     file = cloudpickle.dumps(model_generator)
 
     test_bucket_name()
-    
-    # response = client.post(
-    #     "/models/upload",
-    #     data={"model_name": model_name, "disease": disease, "description": description},
-    #     files={"file": ("test_file.pkl", file, "application/octet-stream")},
-    # )
-    # assert response.status_code == 201, f"Failed with response: {response.text}"
-    # assert response.json()["detail"] == f"Model '{model_name}' registered."
+
+    response = client.post(
+        "/models/upload",
+        data={"model_name": model_name, "disease": disease, "description": description},
+        files={"file": ("test_file.pkl", file, "application/octet-stream")},
+    )
+    assert response.status_code == 201, f"Failed with response: {response.text}"
+    assert response.json()["detail"] == f"Model '{model_name}' registered."
     yield
 
 # def test_download_model(test_upload_model, mock_mlflow_uri):
