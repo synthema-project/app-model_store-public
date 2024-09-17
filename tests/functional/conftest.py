128676a8be1ef0dc2840766d36767f8b82e26388
sblottmann <sbl@gatv.ssr.upm.es>
2024-09-17 16:20:00 +0200

    clean tests and debugs

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,7 +1,6 @@
 import shutil
 import pytest
 from mlflow.pyfunc import PythonModel
-import os
 
 from src import config
 
@@ -9,14 +8,13 @@ from src import config
 def pytest_addoption(parser):
     parser.addoption("--env", action="store", default="local", help="Environment to run tests in: localhost or kubernetes")
 
-def test_bucket_name():
-    assert os.getenv('MLFLOW_S3_BUCKET') == 'mlflow'
 
 @pytest.fixture(scope="session")
 def monkeysession(request):
     mp = pytest.MonkeyPatch()
     yield mp
     mp.undo()
+
 
 @pytest.fixture(scope="session")
 def mock_mlflow_uri(monkeysession, pytestconfig):
@@ -28,13 +26,7 @@ def mock_mlflow_uri(monkeysession, pytestconfig):
     elif env == "local-k8":
         raise NotImplementedError("Kubernetes environment not implemented")
     elif env == "cloud":
-        # Set environment variables for cloud testing
-        monkeysession.setenv("MLFLOW_S3_BUCKET", "mlflow")
-        monkeysession.setenv("MLFLOW_TRACKING_URI", "s3://mflow")
-        # Debugging: Print the environment variables
-        # print(f"MLFLOW_S3_BUCKET: {os.getenv('MLFLOW_S3_BUCKET')}")
-        # print(f"MLFLOW_TRACKING_URI: {os.getenv('MLFLOW_TRACKING_URI')}")
-        test_bucket_name()
+
         yield
     else:
         raise ValueError(f"Unknown environment: {env}")
--- tests/functional/test_f_models.py
+++ tests/functional/test_f_models.py
@@ -1,30 +1,20 @@
 from fastapi.testclient import TestClient
 import cloudpickle
 import pytest
-import os
 
 from src.main import create_app
 
 client = TestClient(create_app())
 
-def test_bucket_name():
-    assert os.getenv('MLFLOW_S3_BUCKET') == 'mlflow'
 
 @pytest.fixture(scope="module")
-def setup_environment():
-    os.environ["MLFLOW_S3_BUCKET"] = "mlflow"
-    yield
-    # del os.environ["MLFLOW_S3_BUCKET"]
-
-@pytest.fixture(scope="module")
-def test_upload_model(model_generator, mock_mlflow_uri, setup_environment):
+def test_upload_model(model_generator, mock_mlflow_uri):
     model_name = "test_model"
     disease = "AML"
     description = "Test description"
 
     file = cloudpickle.dumps(model_generator)
 
-    test_bucket_name()
 
     response = client.post(
         "/models/upload",
@@ -35,17 +25,17 @@ def test_upload_model(model_generator, mock_mlflow_uri, setup_environment):
     assert response.json()["detail"] == f"Model '{model_name}' registered."
     yield
 
-# def test_download_model(test_upload_model, mock_mlflow_uri, setup_environment):
-#     model_name = "test_model"
-#     response = client.get(f"/models/download/{model_name}/1")
-#     assert response.status_code == 200
-#
-# def test_get_model(test_upload_model, mock_mlflow_uri, setup_environment):
-#     model_name = "test_model"
-#     response = client.get(f"/models/{model_name}")
-#     assert response.status_code == 200
-#
-# def test_get_model_versions(test_upload_model, mock_mlflow_uri, setup_environment):
-#     model_name = "test_model"
-#     response = client.get(f"/models/{model_name}/versions")
-#     assert response.status_code == 200
+def test_download_model(test_upload_model, mock_mlflow_uri):
+    model_name = "test_model"
+    response = client.get(f"/models/download/{model_name}/1")
+    assert response.status_code == 200
+
+def test_get_model(test_upload_model, mock_mlflow_uri):
+    model_name = "test_model"
+    response = client.get(f"/models/{model_name}")
+    assert response.status_code == 200
+
+def test_get_model_versions(test_upload_model, mock_mlflow_uri):
+    model_name = "test_model"
+    response = client.get(f"/models/{model_name}/versions")
+    assert response.status_code == 200
