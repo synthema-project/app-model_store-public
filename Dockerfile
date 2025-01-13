4b482cb6a385994e3a33e94169a7269f430c6571
Borja Arroyo Galende <bag@gatv.ssr.upm.es>
2025-01-13 11:05:23 +0100

    Breaking changes: bitnami helm chart

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM tiangolo/uvicorn-gunicorn-fastapi:python3.11-slim
-WORKDIR /app
-COPY src/requirements.txt src/requirements.txt
-RUN pip install -r src/requirements.txt
-COPY . .
-EXPOSE 8001
-CMD ["python", "-m", "src.main"]
--- src/config.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-import os
-
-def load_mlflow_client():
-    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
-    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-    return mlflow.MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
-
-mlflow_client = load_mlflow_client()
